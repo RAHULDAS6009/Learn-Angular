0cafe1c54e4d268a2bd10a2559af0627225b64b7
RAHULDAS6009 <das206053@gmail.com>
2025-02-03 21:47:18 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,TasksComponent],
+  imports: [HeaderComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: TasksComponent },
-
+  { path: 'about', component: AboutComponent },
 ];
 
-  // lazy loading
-  // {path: 'users',
-  //     loadChildren: () => import('./users/users.routes').then(m => m.userRoutes) }
-  /*
+// lazy loading
+// {path: 'users',
+//     loadChildren: () => import('./users/users.routes').then(m => m.userRoutes) }
+/*
     import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list.component';
 import { UserDetailComponent } from './user-detail.component';
