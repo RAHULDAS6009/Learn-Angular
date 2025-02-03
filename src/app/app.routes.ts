6437e135978fddd1f82a5511d1bd4a479322294f
RAHULDAS6009 <das206053@gmail.com>
2025-02-03 21:33:20 +0530

    how to add route and lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TasksComponent },
+
+];
+
+  // lazy loading
+  // {path: 'users',
+  //     loadChildren: () => import('./users/users.routes').then(m => m.userRoutes) }
+  /*
+    import { Routes } from '@angular/router';
+import { UserListComponent } from './user-list.component';
+import { UserDetailComponent } from './user-detail.component';
+
+export const userRoutes: Routes = [
+  { path: '', component: UserListComponent },
+  { path: ':id', component: UserDetailComponent }
+];
+     */
