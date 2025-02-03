c3c0cf05349f83eebfd7b040c8b823c42c2c7e0a
RAHULDAS6009 <das206053@gmail.com>
2025-02-03 20:29:07 +0530

    concept of Subject

---
+++ src/app/services/ui.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+/*A Subject in RxJS is a special type of Observable that allows values to be multicasted 
+(i.e., multiple subscribers receive the same data). 
+It acts as both an Observable and an Observer. */
+
+/*
+1. Why Use a Subject?
+Unlike a normal Observable, a Subject allows multiple subscribers to receive the same data.
+It can emit values manually using .next().
+Used for cross-component communication in Angular.
+ */
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UiService {
+  private showAddTask:boolean=false;
+  private subject=new Subject<any>()
+  constructor() {}
+}
